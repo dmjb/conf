3e6eaa98615a5460ed2ce062d888c48f42ae1ca6
Don Browne <dmjb@users.noreply.github.com>
2023-05-25 10:55:03 +0100

    Update vim config (#1)

--- vim/bootstrap.sh
+++ vim/bootstrap.sh
@@ -3,9 +3,4 @@
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s $(pwd)/vimrc $HOME/.vimrc
 vim +PluginInstall
-$HOME/.vim/bundle/YouCompleteMe/install.py
-cd $HOME/.vim/bundle/vimproc.vim/ && make
 
-git clone https://github.com/powerline/fonts /tmp/powerline-fonts
-/tmp/powerline-fonts/install.sh
-rm -rf /tmp/powerline-fonts
